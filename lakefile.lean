917f8e056287529b4122f412cb78a5e04f897ffe
wadkisson <adkisson@wustl.edu>
2026-05-30 17:43:30 -0500

    fix

--- lakefile.lean
+++ lakefile.lean
@@ -195,7 +195,7 @@ require Comparator from git
   "https://github.com/leanprover/comparator" @ "10033e381ff7f2146859e21ab99ce01f9ed61c36"
 
 require LeanProfiler from git
-  "https://github.com/wadkisson/LeanProfiler" @ "2a2e6ce27e8c6f07892f92a62359d64111663071"
+  "https://github.com/wadkisson/LeanProfiler" @ "a46182bfaa647399b8e168d4fde5a7c1f3b0df1e"
 
 -- Keep `mathlib` last so Mathlib’s dependency versions win, which is required for cache tooling.
 require mathlib from git
